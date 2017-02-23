9b2077089ede2239061d965eb005063047e7b735
Evan Haskell <ehaskell@oakland.edu>
2017-02-22 22:17:28 -0500

    Test working SQLite driver

--- src/main/java/com/github/mawan5512/pmd/PMD.java
+++ src/main/java/com/github/mawan5512/pmd/PMD.java
@@ -1,9 +1,34 @@
 package com.github.mawan5512.pmd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PMD {
 
     public static void main(String[] args) {
-        System.out.println(com.mysql.jdbc.Driver.class);
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:///home/evan/Desktop/database", "root", "");
+        } catch (SQLException e) {
+            System.err.println("Could not create connection.");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            connection.createStatement().executeUpdate("test");
+        } catch (SQLException e) {
+            System.err.println("Could not execute command.");
+            e.printStackTrace();
+        }
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Could not close connection.");
+            e.printStackTrace();
+        }
     }
 
 }
