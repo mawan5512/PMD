4927198331f7ccf93bb2877a02a25f55c4066e43
Evan Haskell <ehaskell@oakland.edu>
2017-02-25 23:26:27 -0500

    Use singleton in PMD instead of all static methods

--- src/main/java/com/github/mawan5512/pmd/PMD.java
+++ src/main/java/com/github/mawan5512/pmd/PMD.java
@@ -4,11 +4,20 @@ import com.github.mawan5512.pmd.omdb.OmdbMovieSearcher;
 
 public class PMD {
 
-    public static OmdbMovieSearcher getOmdbMovieSearcher() {
+    private final static PMD singleton = new PMD();
+    private final OmdbMovieSearcher searcher;
+
+    public OmdbMovieSearcher getOmdbMovieSearcher() {
         return searcher;
     }
 
-    private final static OmdbMovieSearcher searcher = new OmdbMovieSearcher(0);
+    private PMD() {
+        searcher = new OmdbMovieSearcher(0);
+    }
+
+    public static PMD getInstance() {
+        return singleton;
+    }
 
     public static void main(String[] args) {
 
